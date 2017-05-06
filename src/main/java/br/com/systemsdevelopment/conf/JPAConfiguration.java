b0524ad95e63db3645d94cc555f0d901c2ea791f
iran junior <iran junior>
2017-05-05 23:21:06 -0300

    configuracao hiroku

--- src/main/java/br/com/systemsdevelopment/conf/JPAConfiguration.java
+++ src/main/java/br/com/systemsdevelopment/conf/JPAConfiguration.java
@@ -28,9 +28,12 @@ public class JPAConfiguration {
 	public DataSource dataSource(){
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("org.postgresql.Driver");
-		dataSource.setUrl("jdbc:postgresql://localhost:5432/Biblioteca");
-		dataSource.setUsername("postgres");
-		dataSource.setPassword("iran@syber");
+		//dataSource.setUrl("jdbc:postgresql://localhost:5432/Biblioteca");
+		dataSource.setUrl("jdbc:postgres://ynydsdtebnbsvh:610f7b3b5e95fa9882616e5098e5e16f69d4ee0045ced203e3c7efd7ca2632a2@ec2-23-21-111-81.compute-1.amazonaws.com:5432/deol703kpl3t17");
+		//dataSource.setUsername("postgres");
+		dataSource.setUsername("ynydsdtebnbsvh");
+		//dataSource.setPassword("iran@syber");
+		dataSource.setPassword("610f7b3b5e95fa9882616e5098e5e16f69d4ee0045ced203e3c7efd7ca2632a2");
 		return dataSource;
 	}
 	
